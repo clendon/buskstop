9681f7f84c9329ea7cc3d5ab95c81e6e1f9dc268
Melika Campbell <m3likas@gmail.com>
2021-08-27 14:06:25 -0700

    added routes

--- server/app.js
+++ server/app.js
@@ -1,25 +1,30 @@
-const express = require('express');
+const PORT = 3000
 const path = require('path');
-
-const app = express();
-
-
-// configuration
-const port = 3000;
-
-
-//middleware
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./db/db.js')
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const jsonParser = bodyParser.json()
+const app = express()
+app.use(jsonParser)
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))
-
-
-// routes
-app.get('/', (req, res) => {
-  res.send('Hello Test!');
-});
-
-app.listen(port, () => {
-  console.log(`app listening on port${port}`)
-});
-
+app.get('/people', (req, res) => {
+  db.models.people.find()
+  .exec()
+  .then((data) => {
+    res.json(data)
+  })
+  .catch((err) => {
+    console.log('you have an err', err)
+    res.end()
+  })
+})
+app.post('/people', (req, res) => {
+  res.sendStatus(201)
+})
+app.listen(PORT, () => {
+  console.log(`Server is listening on Port:${PORT}`)
+})
 // app basic functionality is up and running
