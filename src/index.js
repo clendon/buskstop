2b6442b0c59d065be4f7a8ad9eed7f32ec045ee8
Sean <smcgovern135@gmail.com>
2021-08-28 11:21:38 -0400

    importing main.css from assets in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import './assets/main.css'
 
 //TODO: render an actual React Component
 ReactDOM.render(<div>Testing</div>, document.getElementById('app'));
