11c0aa3026ba9fd6a98e99294d42e321163ae114
Melika Campbell <m3likas@gmail.com>
2021-08-28 08:24:16 -0700

    increased readability

--- server/app.js
+++ server/app.js
@@ -1,24 +1,28 @@
-const PORT = 3000
-//configuration
-const path = require('path');
-//path to database
-const db = require('../db/db.js')
-//middleware
 const express = require('express')
-const bodyParser = require('body-parser')
 const mongo = require('mongodb');
-const MongoClient = mongo.MongoClient;
-const jsonParser = bodyParser.json()
+const path = require('path');
+
+
+// path to database
+const db = require('../db/db.js')
+
+// create server
 const app = express()
-app.use(jsonParser)
+
+// configuration
+const PORT = 3000
+
+// middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
-//routes
+
+
+// routes
 app.get('/people', (req, res) => {
   db.models.people.find()
   .exec()
   .then((data) => {
-    res.json(data)
+    res.send(data)
   })
   .catch((err) => {
     console.log('you have an err', err)
@@ -28,7 +32,8 @@ app.get('/people', (req, res) => {
 app.post('/people', (req, res) => {
   res.sendStatus(201)
 })
+
+// starting the server
 app.listen(PORT, () => {
   console.log(`Server is listening on Port:${PORT}`)
 })
-// app basic functionality is up and running
