4357762b985b12a352e9fddc219c036d4208e2f7
Sean <smcgovern135@gmail.com>
2021-08-28 11:52:32 -0400

    reactDOM now renders App.jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './assets/main.css'
-
+import App from './App.jsx'
 //TODO: render an actual React Component
-ReactDOM.render(<div>Testing</div>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
