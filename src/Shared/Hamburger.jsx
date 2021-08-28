555ddef8951dc0842975d73b181bb294e0b75e05
Sean <smcgovern135@gmail.com>
2021-08-28 16:41:36 -0400

    init

---
+++ src/Shared/Hamburger.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+const Hamburger = ({options}) => {
+  const [open, setOpen] = useState(false);
+
+  const clickHandler == () => {};
+
+  const redirect = () => {};
+
+  return (
+    <div className="hidden mobile-menu"></div>
+  );
+
+};
+
+export default Hamburger;
