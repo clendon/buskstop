408927f03473cb33362365e2074fb4d67814dded
Sean <smcgovern135@gmail.com>
2021-08-30 10:22:13 -0400

    basic menu/onclick functionality added

--- src/Shared/Hamburger.jsx
+++ src/Shared/Hamburger.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment} from 'react';
 
 const Hamburger = ({options}) => {
   const [open, setOpen] = useState(false);
@@ -10,15 +10,35 @@ const Hamburger = ({options}) => {
   const redirect = () => {};
 
   return (
-    <button onClick={clickHandler}>
-      <svg viewBox="0 0 100 80" width="40" height="40">
-        <rect width="100" height="20"></rect>
-        <rect y="30" width="100" height="20"></rect>
-        <rect y="60" width="100" height="20"></rect>
-      </svg>
-    </button>
+    <>
+      {open && 
+        <div className="flex flex-col">
+          <a href="#">map</a>
+          <a href="#">feed</a>
+          <a href="#">your performances</a>
+          <a href="#">search all buskers</a>
+          <a href="#">logout</a>
+        </div>
+      }
+      <button onClick={clickHandler}>
+        <svg viewBox="0 0 100 80" width="40" height="40">
+          <rect width="100" height="20"></rect>
+          <rect y="30" width="100" height="20"></rect>
+          <rect y="60" width="100" height="20"></rect>
+        </svg>
+      </button>
+    </>
   );
 
 };
 
 export default Hamburger;
+
+
+      // <div>
+      //   <a href=#>map</a>
+      //   <a href=#>feed</a>
+      //   <a href=#>your performances</a>
+      //   <a href=#>buskers</a>
+      //   <a href=#>logout</a>
+      // </div>
