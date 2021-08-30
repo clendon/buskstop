3fe437c6db721d044c20c1ca61c103148c5faf78
Sumit Patel <sumitz1207@yandex.com>
2021-08-30 13:00:18 -0400

    working map with markers and popouts using sampleData

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import MapView from './Map/MapView.jsx'
 
 //central React App component
 //random tailwind adds to test functionality
 const App = () => {
   return (
-      <div className="max-w-sm rounded-shadow border-4">testing</div>
+      <div className="max-w-sm rounded-shadow border-4">testing <MapView /></div>
   );
 };
 
--- src/Map/MapView.jsx
+++ src/Map/MapView.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import Map from "./Map";
+import Map from "./Map.jsx";
 
 function MapView(props){
     return(
