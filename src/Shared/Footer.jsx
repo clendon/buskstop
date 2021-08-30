f10869ebffb483e1846f5b4d4eeef1b2be41403e
Sean <smcgovern135@gmail.com>
2021-08-30 15:01:16 -0400

    initialized dummy footer div

---
+++ src/Shared/Footer.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+//import Hamburger from './Shared/Hamburger.jsx';
+
+const Footer = ({menu}) => {
+  return (
+    <div className="border border-black w-full">Footer Here</div>
+  );
+};
+
+export default Footer;
