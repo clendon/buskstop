81feeb0e6a609244fc6d7dc4c48088b0d65b66dc
Sean <smcgovern135@gmail.com>
2021-08-30 08:42:58 -0400

    added svg hamburger

--- src/Shared/Hamburger.jsx
+++ src/Shared/Hamburger.jsx
@@ -3,12 +3,20 @@ import React, {useState} from 'react';
 const Hamburger = ({options}) => {
   const [open, setOpen] = useState(false);
 
-  const clickHandler = () => {};
+  const clickHandler = () => {
+    setOpen(!open);
+  };
 
   const redirect = () => {};
 
   return (
-    <div className="mobile-menu border-1"></div>
+    <button onClick={clickHandler}>
+      <svg viewBox="0 0 100 80" width="40" height="40">
+        <rect width="100" height="20"></rect>
+        <rect y="30" width="100" height="20"></rect>
+        <rect y="60" width="100" height="20"></rect>
+      </svg>
+    </button>
   );
 
 };
