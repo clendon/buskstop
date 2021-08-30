3727ded627996306ed404120656e8fa0b3774574
Sean <smcgovern135@gmail.com>
2021-08-30 16:55:37 -0400

    set up basic React Router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import {Route, HashRouter as Router, Switch, Link} from 'react-router-dom';
+import Header from './Shared/Header.jsx';
+import Footer from './Shared/Footer.jsx';
+import Search from './Search/Search.jsx';
 
-//central React App component
-//random tailwind adds to test functionality
-const App = () => {
-  return (
-      <div className="max-w-sm rounded-shadow border-4">testing</div>
-  );
-};
+const App = () => (
+  <Router>
+    <div className="h-full shadow border flex flex-col justify-between">
+      <Switch>
+        <Route exact path="/audience/:id">
+          {/* filler */}
+        </Route>
+        <Route exact path="/login">
+          {/* filler */}
+        </Route>
+        <Route exact path="/map">
+          {/* filler */}
+        </Route>
+        <Route exact path="/performer/:id">
+          {/* filler */}
+        </Route>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+);
 
 export default App;
