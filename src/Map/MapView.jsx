2bc40942d1df4cfaa96db3db9caf9bdfa74d4641
Sumit Patel <sumitz1207@yandex.com>
2021-08-30 08:07:39 -0400

    added map components with sample data and working markers

---
+++ src/Map/MapView.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import Map from "./Map";
+
+function MapView(props){
+    return(
+        <section>
+            <Map />
+        </section>
+    );
+}
+
+export default MapView;
