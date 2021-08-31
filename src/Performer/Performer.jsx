11a608c79ae1ffa459f976eebcbb2a9f806ae356
Hayden Bonnen <HaydenBP@Gmail.com>
2021-08-31 09:21:58 -0400

    saving to merge

--- src/Performer/Performer.jsx
+++ src/Performer/Performer.jsx
@@ -3,17 +3,16 @@ import PerformerInfo from './PerformerInfo.jsx';
 import PerformerTile from './PerformerTile.jsx';
 
 const Performer = (props) => {
-
-  const performances = [1,2,3]
-
+  const performances = [1, 2, 3];
 
   return (
     <div>
-      <PerformerInfo/>
-      {performances.map(performance => <PerformerTile/>)}
+
+      <PerformerInfo />
+      {performances.map(() => <PerformerTile />)}
     </div>
 
   );
-}
+};
 
 export default Performer;
--- src/Performer/PerformerInfo.jsx
+++ src/Performer/PerformerInfo.jsx
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
 
-const PerformerInfo = (props) => {
-  var name = 'name'
-  var location = 'location'
-  var image = 'https://iconarchive.com/download/i61331/majdi-khawaja/shrek/Shrek-2.ico'
+const PerformerInfo = () => {
+  const name = 'name';
+  const role = 'role';
+  const image = 'https://iconarchive.com/download/i61331/majdi-khawaja/shrek/Shrek-2.ico';
+  const followers = 5 + ' Followers'
   return (
-    <div className='w-screen h-1/6 bg-red-300'>
-    <div className='flex flex-row min-w-full bg-purple-600'>
-    <div className='flex justify-center items-center w-2/6 bg-gray-200'>
-        <div><img src={image}/></div>
+    <div className="w-screen  bg-red-300">
+      <div className="flex flex-col justify-center items-center content-center bg-green-500">
+        <div>
+          <img src={image}/>
         </div>
-      <div className='flex flex-col w-4/6 bg-green-500'>
-        <div className=' bg-green-400'>
-          <div>{name}</div>
-        </div>
-        <div className=''>
-          <div>{location}</div>
-        </div>
+        <div>{name}</div>
+        <div>{role}</div>
+        <div>{followers}</div>
       </div>
-
     </div>
-  </div>
-
   );
-}
+};
 
 export default PerformerInfo;
