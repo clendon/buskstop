c2f0e26f68614eca24ebeceedc8c939795de63e7
Sean <smcgovern135@gmail.com>
2021-08-30 23:14:17 -0400

    added grid-flow-col-dense to app; hopefully will auto-place items correctly

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './Shared/Footer.jsx';
 import Search from './Search/Search.jsx';
 
 const App = () => (
-    <div className="h-screen grid">
+    <div className="h-screen grid grid-flow-col-dense">
       <Router>
         <Switch>
           <Route exact path="/audience/:id">
