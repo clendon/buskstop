2fd4c18f5e51be9f98f9f29a7014bca448023bce
Joshua Barnes <josh.barnes003@gmail.com>
2021-08-31 16:11:33 -0400

    Search Component is workable

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Busker from './Components/Busker.jsx';
+import params from './variables.js';
 
 const Search = function() {
+  const [buskers, setBuskers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/${params.people}`)
+      .then(({ data }) => {
+        setBuskers(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(buskers);
+  }, [buskers]);
+
   return (
-    <div>This is the Search Page</div>
-  )
+    <div id="search-page" className="w-screen h-full max-h-full">
+      <div>This will be the search bar component</div>
+      <div id="search-display" className="max-h-full overflow-scroll">
+        {buskers.map((busker) => <Busker key={busker.ID} busker={busker} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
