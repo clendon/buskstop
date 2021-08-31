2e11c383d8dfe30a8810a3a69f9e764c33e20253
Sean <smcgovern135@gmail.com>
2021-08-31 15:58:24 -0400

    created initial card component

--- src/Shared/Feed.jsx
+++ src/Shared/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 //this component assumes that it's getting passed a 'performances' prop which is an array of objects
-//for now, it also assumes that each 'performance' looks like the sample object below
+//for now, it also assumes that each 'performance' looks like the sample objects below
 //it also assumes a 'performer' prop which is true if it's getting displayed on a performer's home page (as opposed to an audience member's)
 
 const samplePerformances = [
@@ -26,9 +26,10 @@ const samplePerformances = [
     time: '08:30PM'
   },
 ];
+
 const Feed = ({performances, performer}) => {
   return (
-    <div>
+    <div className="border border-black rounded flex flex-col justify-center">
       {performer && 
         <button className="border border-black rounded">Add a Performance</button>
       }
@@ -41,8 +42,12 @@ const Feed = ({performances, performer}) => {
 
 const Card = ({performance}) => {
   return (
-    <div>
-      
+    <div className="border border-black rounded flex flex-wrap justify-evenly">
+      <p>{performance.name}</p>
+      <p>{performance.location}</p>
+      <p>{performance.category}</p>
+      <p>{performance.date}</p>
+      <p>{performance.time}</p>
     </div>
   ); 
 };
