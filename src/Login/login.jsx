9b1741e3c740ded33ec44a7e8a65e25cb87f88a6
Sean <smcgovern135@gmail.com>
2021-09-01 19:43:45 -0400

    commented out signup + gave inputs wider borders

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Modal from '../Shared/Modal';
+import Modal from '../Shared/Modal.jsx';
 
 export default function Login() {
   const [signUpUsername, setSignUpUsername] = useState('');
@@ -43,20 +43,20 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col border border-black h-full justify-evenly items-center">
-      <form className="border-2 border-black flex flex-col justify-between">
+    <div className="flex flex-col h-full justify-evenly items-center">
+      {/*<form className="border-2 border-black flex flex-col justify-between">
         <h3>SignUp</h3>
         <label for="username">Username:</label>
         <input autofocus className="border border-yellow-600 rounded" type="text" onChange={(e) => setSignUpUsername(e.target.value)} name="username"/>
         <label>Password:</label>
         <input className="border border-yellow-600 rounded" type="text" onChange={(e) => setSignUpPassword(e.target.value)} />
-      </form>
+      </form>*/}
 
-      <form className="flex flex-col w-3/4">
+      <form className="flex flex-col w-3/4 h-1/4 justify-evenly">
         <label>Username:</label>
-        <input className="border border-yellow-600 rounded" type="text" onChange={(e) => setLoginUsername(e.target.value)} />
+        <input className="border-2 border-yellow-600 rounded" type="text" onChange={(e) => setLoginUsername(e.target.value)} />
         <label>Password:</label>
-        <input className="border border-yellow-600 rounded" type="text" onChange={(e) => setLoginPassword(e.target.value)} />
+        <input className="border-2 border-yellow-600 rounded" type="text" onChange={(e) => setLoginPassword(e.target.value)} />
       </form>
 
         <button className=" h-10 bg-yellow-600 rounded w-3/4" onClick={getUser}>Submit</button>
