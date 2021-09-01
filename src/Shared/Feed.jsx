48c315f626ac60a488485e0950919adf0c2f98f5
Sean <smcgovern135@gmail.com>
2021-09-01 08:48:41 -0400

    removed button from Feed component since that should go in overall profile

--- src/Shared/Feed.jsx
+++ src/Shared/Feed.jsx
@@ -1,18 +1,30 @@
-import React from 'react'; //eslint-disable-line
+import React, { useState } from 'react'; //eslint-disable-line
+import Modal from './Modal.jsx' //eslint-disable-line
 // this component assumes that it's getting passed a performances prop which is an array of objects
-// for now, it also assumes that each 'performance' looks like the sample objects below
+// for now, it also assumes that each 'performance' looks like the sample objects in App
 // it also assumes a performer prop which is true if user is performer
 
-const Feed = ({ performances, performer }) => (
-  <div className="border border-black rounded flex flex-col justify-center">
-    {performer &&
-      <button className="border border-black rounded" type="button">Add a Performance</button>
-    }
-    {performances && performances.map((item, index) => (
-      <Card performance={item} key={index} />
-    ))}
-  </div>
-);
+const Feed = ({ performances, performer }) => {
+  const [showModal, setShowModal] = useState(false);
+  let modalContent = performances[0];
+
+  const modalHandler = (e) => {
+    // modalContent = performances[index];
+    // setShowModal(!showModal);
+    e.preventDefault();
+    console.log('modal here!');
+  };
+
+  return (
+    <>
+      <div className="border border-black rounded flex flex-col justify-center">
+        {performances && performances.map((item, index) => (
+          <Card performance={item} key={index} onClick={modalHandler} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 const Card = ({performance}) => (
   <div className="border border-black rounded flex flex-wrap justify-evenly">
