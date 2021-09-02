f881f70771c5b91696fb4466e077986ae3f37918
Sean <smcgovern135@gmail.com>
2021-09-02 17:39:58 -0400

    important layout changes

--- src/Shared/Shrek.jsx
+++ src/Shared/Shrek.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 // DONKEY
-const Shrek = () => (
-  <div>I'M AN OGRE</div>
-);
+const Shrek = () => {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/profile/Shrek/busker`)
+    .then(res => res.json())
+    .then(data => setUrl(data[0].image))
+    .catch(err => console.error('error in the swamp', err));
+  }, []);
+
+  return (
+    <div className="grid w-full h-1/2 place-items-center">
+      <img src={url} alt="an image of Shrek from the movie series" />
+    </div>
+  );
+};
 
 export default Shrek;
