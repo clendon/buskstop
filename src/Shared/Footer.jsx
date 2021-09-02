b91b7905fa17a783cda8eeda29efb42f796dc076
Joshua Barnes <josh.barnes003@gmail.com>
2021-09-02 17:41:36 -0400

    changed hooter for lg screens

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -4,8 +4,8 @@ import Hamburger from './Hamburger.jsx';
 
 const Footer = ({menu}) => {
   return (
-    <div className="grid grid-rows-1 grid-cols-1 gap-0 justify-self-end items-end justify-items-end bg-yellow-600 fixed bottom-0 right-0 w-full">
-      {menu && <Hamburger />}
+    <div className="grid grid-rows-1 grid-cols-1 gap-0 justify-self-end items-end justify-items-end bg-yellow-600 fixed bottom-0 right-0 w-full lg:h-16">
+      {menu && <Hamburger hidden={'lg:hidden'} />}
     </div>
   );
 };
