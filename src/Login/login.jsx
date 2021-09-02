abf06b979aa77f3e259ad114e86658b7b2abb399
Sean <smcgovern135@gmail.com>
2021-09-02 11:55:06 -0400

    made modal component functional + styled

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -47,17 +47,16 @@ export default function Login() {
       {showModal && 
         (
           <Modal onClick={(e) => {setShowModal(false)}}> 
-            <p>hi</p>
+            <form className="flex flex-col justify-evenly w-3/4">
+              <label for="username">Username:</label>
+              <input className="border-2 border-yellow-600 rounded" type="text" onChange={(e) => setSignUpUsername(e.target.value)} name="username"/>
+              <label>Password:</label>
+              <input className="border-2 border-yellow-600 rounded" type="text" onChange={(e) => setSignUpPassword(e.target.value)} />
+            </form>
+            <button type="submit" className=" h-10 bg-yellow-600 rounded w-3/4" onClick={getUser}>Register</button>
           </Modal>
         )
       }
-            {/*<form className="border-2 border-black flex flex-col justify-between">
-              <h3>SignUp</h3>
-              <label for="username">Username:</label>
-              <input className="border border-yellow-600 rounded" type="text" onChange={(e) => setSignUpUsername(e.target.value)} name="username"/>
-              <label>Password:</label>
-              <input className="border border-yellow-600 rounded" type="text" onChange={(e) => setSignUpPassword(e.target.value)} />
-            </form>*/}
       <form className="flex flex-col w-3/4 h-1/4 justify-evenly">
         <label>Username:</label>
         <input className="border-2 border-yellow-600 rounded" type="text" onChange={(e) => setLoginUsername(e.target.value)} />
@@ -65,7 +64,7 @@ export default function Login() {
         <input className="border-2 border-yellow-600 rounded" type="text" onChange={(e) => setLoginPassword(e.target.value)} />
       </form>
       <div className="flex flex-col items-center justify-around h-1/6">
-        <button className=" h-10 bg-yellow-600 rounded w-3/4" onClick={getUser}>Submit</button>
+        <button type="submit" className=" h-10 bg-yellow-600 rounded w-3/4" onClick={getUser}>Submit</button>
         <button className="underline text-xs" onClick={() => {setShowModal(true)}}>Not signed up yet? Click here to register.</button>
       </div>
     </div>
--- src/Shared/Modal.jsx
+++ src/Shared/Modal.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+// in order to make this modal close, you need to pass it an onClick that does so!
 
 const Modal = ({children, onClick}) => (
-  <dialog className="z-50 absolute border border-yellow-600 bg-white h-1/2 w-1/2" open onClick={onClick}>{children}</dialog>
+  <dialog className="z-50 absolute border border-yellow-600 bg-white h-1/2 w-1/2 flex flex-col justify-evenly items-center" open> 
+    {children}
+    <button onClick={onClick} className="border-2 absolute bottom-1 right-1 rounded border-yellow-600 p-0.5">close</button>
+  </dialog>
 );
 
 export default Modal;
